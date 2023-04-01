b5a5737315f3890443de299615cc6279e05487f6
danielkirwa <kirwadan03@gmail.com>
2023-04-01 10:57:17 +0300

    first commit

---
+++ dashboard.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div>
+    <div class="banner">
+    	<div class="vender-logo"><img src="assets/logo/juelgaicon.png"> </div>
+    	<div class="shop-name"><center><h3>Business name here</h3></center></div>
+    </div>
+</div>
+<!-- nav -->
+<div class="navbar">
+	  <a href="#home">Dashboard</a>
+  <div class="subnav">
+    <button class="subnavbtn">My Business <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#company">Add Business</a>
+      <a href="#team">Add Customer</a>
+      <a href="#careers">Add Supplier</a>
+    </div>
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">Product <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#bring">Add Product</a>
+      <a href="#deliver">Add Units</a>
+    </div>
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">Stock <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#link1">Add Stock</a>
+      <a href="#link2">Adjust Stock</a>
+    </div>
+  </div>
+   <div class="subnav">
+    <button class="subnavbtn">Register <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#link1">Add Staff</a>
+      <a href="#link2">Add Role</a>
+      <a href="#link3">Create Acount</a>
+      <a href="#link4">Link 4</a>
+    </div>
+  </div>
+  <div class="subnav">
+    <button class="subnavbtn">Reports <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#link1">Business Report</a>
+      <a href="#link2">Salers Report</a>
+      <a href="#link3">Orders Report</a>
+      <a href="#link4">Sales Report</a>
+    </div>
+  </div>
+  <a href="#contact">Settings</a>
+  <a href="#contact">Sale Desk</a>
+  <a href="#contact">Username</a>
+</div>
+<!-- end nav -->
+
+<!-- start of dashboard -->
+
+<div>Welcome</div>
+
+<!-- end of dashboard -->
+<!-- footer starts -->
+<div class="footer">
+	 <h3></h3>
+   Juelga solution &copy;  &nbsp;&nbsp;<a href="https://www.juelgasolutions.co.tz" style="text-decoration: none; color: #ffffff">juelgasolution </a><br>
+   <h3></h3>
+</div>
+</body>
+</html>
